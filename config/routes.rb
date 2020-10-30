475489acc248dffcc5d81e75b1a3ea42b438c81a
深山　俊 <macnel@fukayamashunnoMacBook-Pro.local>
2020-10-30 21:48:58 +0900

    devise-user&admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+	namespace :admins do
+  		devise_for :admins, :controller => {
+	      sessions: 'admin_users/sessions',
+	      registrations: 'admin_users/registrations',
+	      passwords: 'admin_users/passwords'
+  		}
+  	end
+  	devise_for :users, :controllers => {
+      sessions: 'users/sessions',
+      registrations: 'users/registrations',
+      passwords: 'users/passwords'
+  	}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
